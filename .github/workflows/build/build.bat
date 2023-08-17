85b19792742d40c39a35cfb1bf4c71e7b6000de9
sydneyowl <pauseplay@126.com>
2023-08-17 19:47:44 +0800

    fix ci

--- .github/workflows/build/build.bat
+++ .github/workflows/build/build.bat
@@ -1,6 +1,6 @@
 @echo off
 md tmp
 dotnet publish -c Release -r win-x86 /p:PublishSingleFile=true
-move /Y bin\Release\net7.0\win-x86\publish\SHX8800.exe ..\tmp\SHX8800_x86.exe
+move /Y .\bin\Release\net7.0\win-x86\publish\SHX8800.exe .\tmp\SHX8800_x86.exe
 dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true
-move bin\Release\net7.0\win-x64\publish\SHX8800.exe ..\tmp\SHX8800_x64.exe
+move /Y .\bin\Release\net7.0\win-x64\publish\SHX8800.exe .\tmp\SHX8800_x64.exe
